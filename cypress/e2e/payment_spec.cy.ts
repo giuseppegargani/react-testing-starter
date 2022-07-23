7f5983a3fe7857cfda5ba1c31907e6194cad3852
giuseppegargani <sentierodisole1@yahoo.it>
2022-07-23 18:42:03 +0200

    aggiunta

--- cypress/e2e/payment_spec.cy.ts
+++ cypress/e2e/payment_spec.cy.ts
@@ -16,15 +16,23 @@ describe('empty spec', () => {
   - SE non prende l'Url Base si puo' anche inserire hardcoded!!!! come nel caso seguente!!!!
 
   - Ma le query in Cypress sono tutte find (e non get): FINDBYROLE!!!!
+
+  - Lo screener di Cypress ha anche un Selettore (a forma di mirino in alto a sinistra!!)
+
 */
 describe('payment', ()=>{
   it('user can make payment', ()=>{
     // login
     cy.visit('http://localhost:3000');
-    cy.findByRole('textbox', {name: /username/i}).type("johndoe");
-    cy.findByLabelText('/password/i').type("s3cret");
+    cy.findByRole('textbox', { name: /username/i }).type('johndoe');
+    cy.findByLabelText(/password/i).type('s3cret');
+    cy.findByRole('checkbox', {name: /remember me/i}).check();
+    cy.findByRole('button', {name: /sign in/i}).click(); 
 
     // check account balance
+    let oldBalance
+    cy.get('[data-test="sidenav-user-balance"]').then($balance => oldBalance = $balance.text()).then(balance => console.log(balance));
+
     // click on pay button
     // search for user
     // add amount and note and click pay 
