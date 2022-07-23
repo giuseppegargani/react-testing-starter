bea4f32aa9d78495c6bc6a64577cf09a72d26e1c
giuseppegargani <sentierodisole1@yahoo.it>
2022-07-23 16:37:06 +0200

    aggiornati

---
+++ cypress/e2e/payment_spec.cy.ts
@@ -0,0 +1,32 @@
+/* Dava di default le seguenti linee di codice:
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+}) */
+
+/* CONCETTI:
+  - MEGLIO INSTALLARE TESTING PLAYGROUND SU CHROME!!!
+    https://chrome.google.com/webstore/detail/testing-playground/hejbmebodbijjdhflfknehhcgaklhano
+  
+  - MEGLIO ESEGUIRE IN ELECTRON IL TESTING!!!
+
+  - SI PUO' USARE L'ESTENSIONE DI CHROME CHE SUGGERISCE QUERY
+
+  - SE non prende l'Url Base si puo' anche inserire hardcoded!!!! come nel caso seguente!!!!
+
+  - Ma le query in Cypress sono tutte find (e non get): FINDBYROLE!!!!
+*/
+describe('payment', ()=>{
+  it('user can make payment', ()=>{
+    // login
+    cy.visit('http://localhost:3000');
+    cy.findByRole('textbox', {name: /username/i}).type("johndoe");
+    crypto.findByLabelText('')
+
+    // check account balance
+    // click on pay button
+    // search for user
+    // add amount and note and click pay 
+  })
+});
