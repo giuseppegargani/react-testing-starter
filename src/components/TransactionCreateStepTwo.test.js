d04b2f1b21c268b57448c1656d9258052adc290e
giuseppegargani <sentierodisole1@yahoo.it>
2022-07-23 16:46:13 +0200

    aggiornamento

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -43,18 +43,39 @@ import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
     - AAA Arrange, Act, Assert!!!
         Arrange prevede la renderizzazione del componente!!, Act prevede che vengano fatte cose
     
+    - INTEGRATION:
+        Si uniscono diversi Unit Test dentro uno solo!!!
+        Che quindi adesso avranno diversi Assert dentro il solito test!!!
+        MA INTEGRATION TEST NON E' SEMPLICEMENTE UNIRE I VARI UNIT TESTS!!!
+        ma c'e' una logica dentro e si devono preparare e puo' prevedere mock module!!!!
+        E poi spesso e' pure meglio avere un unico test lungo invece di tanti tests singoli!!!
+
+    - PER INSTALLARE CYPRESS PER E2E TESTS:
+        yarn add -D cypress @testing-library/cypress
+        yarn run cypress open
+        Si deve fare login sulla dashbpard e poi si devono creare spec di default 
+        esempi di default!!!!!
+        Si possono anche non installare (come da video!!!)
+        e poi si deve installare un comando react testing dentro support di cypress
+        
+    - INSTALLARE REACT TESTING DENTRO CYPRESS
+        si cancella tutto il contenuto dentro cypress/support/commands.ts
+        e si importa import "@testing-library/cypress/add-commands";
+        e si crea un nuovo file di test dentro la cartellina "integration"!!!
 */
 
-test('On Initial Render, the pay button is disabled', async () =>{
+/* test('On Initial Render, the pay button is disabled', async () =>{
     //Qui si renderizza un componente!!
     render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}} />);
 
     expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
-});
+}); */
 
 test('If an amount and note has been inserted, the button becomes enabled!!!', async () =>{
     //Qui si renderizza un componente!!
     render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}} />);
+
+    expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
 
     //expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
     userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
